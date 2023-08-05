0b7bc51d39d3b9fbb22d65ef1c1d2038875df80e
Priyansu67 <25521478+Priyansu67@users.noreply.github.com>
2023-08-06 02:04:30 +0530

    Documented Code

--- server/server.js
+++ server/server.js
@@ -6,8 +6,7 @@ const cliProgress = require("cli-progress");
 
 const app = express();
 app.use(express.json());
-app.use(cors());// Enable CORS to allow requests from the React app
-
+app.use(cors()); // Enable CORS to allow requests from the React app
 
 const maxConcurrentDownloads = 2; //This will limit the number of concurrent downloads to 2
 const downloadQueue = []; // This will hold the download queue
@@ -26,75 +25,81 @@ const multibar = new cliProgress.MultiBar(
 );
 const bars = {}; // This will hold the progress bar instances for each worker thread
 
+// This function will process the download queue and start the download
 const processDownloadQueue = () => {
+  // Check if the download queue is empty and the number of active downloads is less than the maximum concurrent downloads
   while (downloadQueue.length > 0 && activeDownloads < maxConcurrentDownloads) {
-    const { url, destination } = downloadQueue.shift();
-    activeDownloads++;
+    const { url, destination } = downloadQueue.shift(); // Get the first item from the download queue and remove it from the queue
+    activeDownloads++; // Increment the active downloads counter because we are starting a new download
 
+    // Create a new worker thread and use the workerCode from the worker.js file
     const worker = new Worker("./worker.js", {
       eval: false,
+      // Pass the url and destination to the worker thread using the workerData property
       workerData: { url, destination },
     });
 
+    // Create a new progress bar instance for the worker thread with the threadId as the key
     if (!bars[worker.threadId]) {
       bars[worker.threadId] = multibar.create(100, 0, {
-        filename: `Worker ${worker.threadId}`,
+        name: `Worker ${worker.threadId}`,
       });
     }
 
+    // Listen for messages from the worker thread
     worker.on("message", (message) => {
       bars[worker.threadId].start(100, 0, {
         name: `Worker ${worker.threadId}`,
-      });
-      if (message.progress !== undefined) {
-        // update the progress bar
-        bars[worker.threadId].update(message.progress);
-      } else if (message.success) {
-        bars[worker.threadId].update(100);
-        //terminate the particular worker thread
+      }); //Start the progress bar when the worker thread sends a message
 
-        activeDownloads--;
+      //If the message contains the progress property, update the progress bar
+      if (message.progress !== undefined) {
+        bars[worker.threadId].update(message.progress); // Update the progress bar with the progress value sent by the worker thread
+      } else if (message.success) {
+        bars[worker.threadId].update(100);// Update the progress bar to 100% when the download is completed
+        activeDownloads--; // Decrement the active downloads counter to give space for new downloads
       } else {
-        // Log the error message
+        // Log the error message when the download fails
         console.error(
           `Worker ${worker.threadId}: File download failed for ${url}. Error: ${message.error}`
         );
-        // Decrement the active downloads counter
-        activeDownloads--;
+        activeDownloads--; // Decrement the active downloads counter to give space for new downloads on error
       }
       processDownloadQueue(); // Continue processing the download queue
     });
 
+    // Listen for errors from the worker thread
     worker.on("error", (error) => {
-      console.error("Worker error:", error);
-      activeDownloads--;
+      console.error("Worker error:", error);// Log the error message when the worker thread throws an error
+      activeDownloads--;// Decrement the active downloads counter to give space for new downloads on error
       processDownloadQueue(); // Continue processing the download queue
     });
   }
+  // Stop the progress bar when the download queue is empty and there are no active downloads
   if (downloadQueue.length === 0 && activeDownloads === 0) {
-    multibar.stop();
-    console.log("All downloads completed.");
+    multibar.stop();// Stop the progress bar
+    console.log("All downloads completed.");// Log a message to the console to indicate that all downloads are completed
   }
 };
 
 app.post("/api/download", (req, res) => {
-  const { urls } = req.body;
+  const { urls } = req.body;// Get the urls array from the request body
   if (!urls || !Array.isArray(urls)) {
-    return res.status(400).json({ error: "Invalid request body." });
+    return res.status(400).json({ error: "Invalid request body." });// Return an error response if the urls array is not present in the request body or if it is not an array
   }
 
-  const destinationDir = "./downloads";
+  const destinationDir = "./downloads";// Name of the directory where the files will be downloaded
   if (!fs.existsSync(destinationDir)) {
-    fs.mkdirSync(destinationDir);
+    fs.mkdirSync(destinationDir);// Create a new directory if it doesn't exist
   }
 
-  urls.forEach((url, i) => {
-    const destination = `${destinationDir}/file${i}:${new Date().getTime()}`;
-    downloadQueue.push({ url, destination });
+  urls.forEach((url, i) => {// Loop through the urls array
+    const destination = `${destinationDir}/file${i}:${new Date().getTime()}`;// Create a unique file name for each download
+    downloadQueue.push({ url, destination });// Add the download to the download queue
   });
 
   processDownloadQueue(); // Start processing the download queue
-  res.json({ message: "Download added to the queue." });
+  res.json({ message: "Download added to the queue." });// Return a success response so that the client knows that the download has been added to the queue
 });
 
 const port = 3005;
--- server/worker.js
+++ server/worker.js
@@ -1,44 +1,49 @@
 const { parentPort, workerData } = require("worker_threads");
-const axios = require("axios"); // Include axios in the worker thread
+const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 const db = require("mime-db");
 
+// This function will download the file from the url and save it to the destination received from the parent thread as workerData
 const downloadFile = async (url, destination) => {
   try {
     const response = await axios.get(url, {
-      responseType: "arraybuffer",
-      onDownloadProgress: (progressEvent) => {
+      responseType: "arraybuffer", // This is important, otherwise the downloaded data will be in text format instead of binary
+      onDownloadProgress: (progressEvent) => { // This is the progress callback of axios which will be called for every progress update
         let percentCompleted = Math.round(
           (progressEvent.loaded * 100) / progressEvent.total
-        );
-        parentPort.postMessage({ progress: percentCompleted });
+        );// Calculate the percentage of the download completed to update the progress to the parent thread
+        parentPort.postMessage({ progress: percentCompleted }); // Send the progress to the parent thread
       },
     });
 
     if (response.status !== 200) {
       throw new Error(
-        `Failed to download file from ${url}. Status: ${response.status}`
+        `Failed to download file from ${url}. Status: ${response.status}`// Throw an error if download fails
       );
     }
 
-    const contentType = response.headers["content-type"];
+    const contentType = response.headers["content-type"];// Extract the content type from the response headers
+    
+    // Extract the file extension from the mime-db using the content type if it exists
     const fileExt = db[`${contentType}`]
       ? db[`${contentType}`].extensions[0]
-      : "";
+      : ""; //Don't use the file extension if it doesn't exist in the mime-db
+    
     const contentDisposition = response.headers["content-disposition"];
-    const fileExtension = fileExt
+
+    const finalFileExtension = fileExt //Check if we got the file extension from the mime-db
       ? "." + fileExt
-      : path.extname(contentDisposition || url) || "";
+      : path.extname(contentDisposition || url) || ""; // If not then xtract the file extension from the content disposition header or the url or use an empty extension
 
-    // Append the extracted file extension to the destination path
-    const destinationWithExtension = destination + fileExtension;
+    const destinationAndFilenameWithExtension = destination + finalFileExtension;// Append the extracted file extension to the destination path received from the parent thread
 
-    fs.writeFileSync(destinationWithExtension, response.data);
+    fs.writeFileSync(destinationAndFilenameWithExtension, response.data); // Write the downloaded data to the file
 
-    parentPort.postMessage({ success: true });
+    parentPort.postMessage({ success: true }); // Send a success message to the parent thread
   } catch (error) {
-    parentPort.postMessage({ success: false, error: error.message });
+    parentPort.postMessage({ success: false, error: error.message }); // Send an error message to the parent thread
   }
 };
-downloadFile(workerData.url, workerData.destination);
+
+downloadFile(workerData.url, workerData.destination);// Call the downloadFile function with the url and destination received from the parent thread
